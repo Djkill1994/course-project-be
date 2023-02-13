f7d438bee3440a04673474341b28c96920964fc1
Vlad <84898137+Djkill1994@users.noreply.github.com>
2023-02-14 02:12:44 +0300

    Code refactoring

--- src/routes/collection.js
+++ src/routes/collection.js
@@ -3,10 +3,11 @@ const {check, validationResult} = require('express-validator');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const Collection = require("../models/Collection");
+const authMiddleware = require("../middlewares/auth");
 const router = Router();
 
-router.post(
-    '/create',
+router.put(
+    '/',
     [
         check('description').isLength({min: 1}),
         check('name').isLength({min: 1}),
@@ -32,7 +33,6 @@ router.post(
                 date: Date.now(),
             })
             await User.updateOne({_id: currentUser}, {$push: {collections: collection}})
-
             return res.status(200).json({message: 'Collection was created.'});
         } catch (error) {
             res.status(500).json({message: 'Create my collection error', error});
@@ -41,16 +41,40 @@ router.post(
 );
 
 router.get(
-    '/my',
+    '/',
     async (req, res) => {
         try {
             const currentUser = await User.findOne({_id: jwt.verify(req.headers.authorization.split(' ')[1], process.env.JWT_SECRET).userId}).populate("collections");
 
             return res.json(currentUser.collections);
         } catch (error) {
+            res.status(500).json({message: 'Get my collections error', error});
+        }
+    }
+);
+
+router.get(
+    `/:id`,
+    async (req, res) => {
+        try {
+            const currentUser = await User.findOne({_id: jwt.verify(req.headers.authorization.split(' ')[1], process.env.JWT_SECRET).userId}).populate("collections");
+            const id = req.params.id
+            const collection = await currentUser.collections
+            return res.json(collection.findOne({_id: id}));
+        } catch (error) {
             res.status(500).json({message: 'Get my collection error', error});
         }
     }
 );
+
+router.delete('/', authMiddleware, async (req, res) => {
+    try {
+        const {id} = req.body;
+        await Collection.deleteOne({_id: id})
+        res.status(200).json({message: 'Collection has been deleted.'});
+    } catch (error) {
+        res.status(500).json({message: 'Delete collection error', error});
+    }
+})
 
 module.exports = router;
